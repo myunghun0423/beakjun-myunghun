658a60fc43fdf438e50d5361b9339e91fa1341f1
myunghun0423 <74221988+myunghun0423@users.noreply.github.com>
2021-03-18 09:45:53 +0900

    Delete baekjun3.java

--- baekjun3.java
+++
@@ -1,8 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("\\    /\\"); //역슬래시는 문자로 인식이 되어서
-        System.out.println(" )  ( ')");  // 이를 구분하려면 역슬래시 부분 뒤에 하나 더 넣어준다
-        System.out.println("(  /  )");
-        System.out.println(" \\(__)|");
-    }
-}
