f155cf2111e29479b839d4f7eb54439d5232bcb1
myunghun0423 <74221988+myunghun0423@users.noreply.github.com>
2021-03-18 09:37:13 +0900

    Create baekjun5.java

---
+++ baekjun5.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt(); //nextInt로 입력값을 받고
+        
+ 
+        System.out.println(a + b); //두 수를 더해서 출력해준다.
+    }
+}
